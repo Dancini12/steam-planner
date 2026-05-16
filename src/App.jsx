0ad154f2adfdd03c776fc3232bf4027b53c28831
Marcel Dancini <marceldancini@gmail.com>
2026-05-16 08:42:55 -0300

    Restaura gerar nova atividade

--- src/App.jsx
+++ src/App.jsx
@@ -255,6 +255,7 @@ export default function App() {
           onOpenProject={goToProjectEditor}
           onOpenLibrary={goToLibrary}
           onOpenBNCC={goToBNCC}
+          onOpenActivityViewer={goToActivityViewer}
         />
       )}
 
